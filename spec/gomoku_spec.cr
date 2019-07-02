4ff7b19cc1ba86628036de8b88b3e61604a7147e
Hentioe_Cl <me@bluerain.io>
2019-07-02 09:44:13 +0800

    Init

---
+++ spec/gomoku_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Gomoku do
+  # TODO: Write tests
+
+  it "works" do
+    true.should eq(true)
+  end
+end
---
+++ src/gomoku.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Gomoku`
+module Gomoku
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
